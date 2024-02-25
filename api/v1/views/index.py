dbe7abd7b20bd6f4db30154ea8c8bb7113c1fc41
volemy <minayobenevolence0@gmail.com>
2024-02-25 17:58:12 +0000

    updtaes

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -20,13 +20,17 @@ classes = {'amenities': Amenity, 'cities': City,
 
 @app_views.route('/status', methods=['GET'])
 def status():
-    """Retrieves the status of the API"""
+    """
+    Retrieves the status of the API
+    """
     return jsonify({"status": "OK"})
 
 
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def number_objects():
-    """Retrieves the number of each objects by type"""
+    """
+    Retrieves the number of each objects by type
+    """
     return jsonify({
         "amenities": storage.count("Amenity"),
         "cities": storage.count("City"),
