3beed4a9aa5ab87e1b9238e2eec18e841cc44489
volemy <minayobenevolence0@gmail.com>
2024-02-25 20:08:10 +0000

    updates again

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,15 +4,9 @@ Flask Application
 """
 
 from flask import Flask, jsonify, make_response
-import os
-
-# Import storage from models
+import os import getenv
 from models import storage
-
-# Import views from api.v1.views
 from api.v1.views import app_views
-
-# Import CORS from falsk_cors
 from flask_cors import CORS
 
 # Create Flask app
@@ -20,16 +14,17 @@ app = Flask(__name__)
 
 #Register blueprint
 app.register_blueprint(app_views)
+app.url_map.strict_slashes = False
 
 # Enable CORS
-cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
 
 
 #Teardown app context
 @app.teardown_appcontext
 def teardown_db(exception):
     """
-    Closes the storage
+    Closes the storage session after each request
     """
     storage.close()
 
@@ -37,7 +32,7 @@ def teardown_db(exception):
 @app.errorhandler(404)
 def not_found(error):
     """
-    404 Error
+    404 Error message definition
     """
     return make_response(jsonify({"error": "Not found"}), 404)
 
