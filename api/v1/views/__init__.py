c5efcb2342a5538e23f17fe4ec6d111d35aeff78
Brokeice123 <keithmarkomen@gmail.com>
2024-02-26 08:03:46 +0000

    modification

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-This method createss a blueprint instance
-"""
+"""views module Init file"""
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
