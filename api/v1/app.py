b9fa4916f92d0ef9b923ba4b45ab061d6bbdb4ab
Brokeice123 <keithmarkomen@gmail.com>
2024-02-26 08:28:02 +0000

    Retracing steps

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,39 +2,20 @@
 """
 Flask Application
 """
-
-from flask import Flask, jsonify, make_response
-import os
+from flask import Flask
 from models import storage
 from api.v1.views import app_views
-from flask_cors import CORS
 
-# Create Flask app
 app = Flask(__name__)
-
-#Register blueprint
 app.register_blueprint(app_views)
-app.url_map.strict_slashes = False
-
-# Enable CORS
-CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
 
 
-#Teardown app context
 @app.teardown_appcontext
 def teardown_db(exception):
     """
-    Closes the storage session after each request
+    Closes the storage
     """
     storage.close()
-
-
-@app.errorhandler(404)
-def not_found(error):
-    """
-    404 Error message definition
-    """
-    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":
